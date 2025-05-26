e3128db16cff5a6d41cd1e7dc304c271edac70b9
Sridhar <sridhar.elumalai@kanini.com>
2025-05-26 12:24:13 +0530

    first commit

---
+++ String/chkPalindrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingQuestions.String
+{
+    public class chkPalindrome
+    {
+        public string isPalindrome(string str)
+        {
+            string inputStr = "madam";
+
+            string result = new string(inputStr.Reverse().ToString());
+
+            return result;
+        }
+    }
+}
